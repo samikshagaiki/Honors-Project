be409bc8abc814f26d657295fb9285b61a90980c
ritikaj22 <149267594+ritikaj22@users.noreply.github.com>
2024-11-25 18:18:27 +0530

    Update TaskController.php
    Fetching those tasks for authenticated users that are not completed

--- task_mgmt/app/Http/Controllers/TaskController.php
+++ task_mgmt/app/Http/Controllers/TaskController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TaskController extends Controller
 {
-    //
+    //Fetching tasks
+    public function index(){
+        $tasks = Task::where('user_id',Auth::id())
+               ->where('is_completed', false)
+               ->get();
+
+        return view('tasks.index',compact('tasks'));
 }
