9ad2bcd3766230ecdd80d69fd955cd6bd78dff8a
ritikaj22 <jainr_1@rknec.edu>
2024-11-27 03:37:58 +0530

    Updated the app.blade.php file with the navbar and required functionalities

--- task_mgmt/resources/views/layouts/app.blade.php
+++ task_mgmt/resources/views/layouts/app.blade.php
@@ -1,28 +1,41 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="en" data-theme="dark">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Task Manager') }}</title>
+    <title>Task Manager</title>
 
     <!-- Pico.css -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/picocss@1.7.7/dist/pico.min.css">
 </head>
-<body>
-    <header class="container">
-        <!-- Navigation -->
-        @include('layouts.navigation')
-    </header>
-
     <main class="container">
-        <!-- Page Heading -->
-        @isset($header)
-            <header class="mb-4">
-                <h1>{{ $header }}</h1>
-            </header>
-        @endisset
+       <!--Navbar -->
+       <nav>
+            <ul>
+                <li>
+                    <strong>
+                        <a href="{{ url('/') }}" style="font-size: 2.5rem;">Task Manager</a>
+                    </strong>
+                </li>
+            </ul>
+            <ul>
+                @auth
+                    <li><a href="{{ route('tasks.index') }}">My Tasks</a></li>
+                    <li>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST">
+                            @csrf
+                            <button type="submit" class="secondary">Logout</button>
+                        </form>
+                    </li>
+                @else
+                    <li><a href="{{ route('login') }}">Login</a></li>
+                    <li><a href="{{ route('register') }}">Register</a></li>
+                @endauth
+            </ul>
+        </nav>
+
 
         <!-- Page Content -->
         <section>
