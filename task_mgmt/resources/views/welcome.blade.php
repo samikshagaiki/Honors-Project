48720ffc08ebe6386e604823f2aad8a966d13e03
Ritika Jain <149267594+ritikaj22@users.noreply.github.com>
2024-11-27 15:58:21 +0530

    Update welcome.blade.php

--- task_mgmt/resources/views/welcome.blade.php
+++ task_mgmt/resources/views/welcome.blade.php
@@ -4,14 +4,5 @@
 <div class="text-center mt-5">
     <h1>Welcome to Task Manager</h1>
     <p>Effortlessly manage your tasks and stay organized!</p>
-
-    @if (Auth::check())
-        <a href="{{ route('tasks.index') }}" class="btn btn-primary">Go to My Tasks</a>
-    @else
-        <a href="{{ route('login')}}" class="btn btn-primary">Login</a>
-        @if (Route::has('register'))
-            <a href="{{ route('register') }}" class="btn btn-secondary">Register</a>
-        @endif
-    @endif
 </div>
 @endsection
